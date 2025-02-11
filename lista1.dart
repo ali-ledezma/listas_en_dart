4a23d7507b61ca692d387d6961b1afc734ba7fb4
ali-ledezma <a.22308051281229@cbtis128.edu.mx>
2025-02-11 16:53:51 +0000

    hola lista

---
+++ lista1.dart
@@ -0,0 +1,14 @@
+void main() {  
+  List<int> numeros = [10, 20, 30];
+  print("Lista de números enteros: ");
+  print(numeros);
+  print("Primer elemento: ${numeros[0]}");  
+  print("Segundo elemento: ${numeros[1]}");
+  print("Tercer elemento: ${numeros[2]}");
+  print("elementos de l lista usando ciclo for:");
+  for (int i = 0; i < numeros.length; i++) {
+    print(numeros[i]);
+  }
+  
+
+}  
